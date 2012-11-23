1678efe633e71b5f7fed5d582f2de4132fb17019
tollmanz <tollmanz@gmail.com>
2012-11-22 21:59:48 -0800

    Setup plugin header

---
+++ ostrichcize.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Ostrichcize
+Plugin URI: https://github.com/tollmanz/ostrichcize
+Description: Hides PHP errors reported by specified plugins or themes.
+Author: Zack Tollman
+Version: 0.1
+Author URI: http://tollmanz.com
+*/
